821b2cdf9aba6e71bc8d9bc239b261835bd700f3
John Wilkinson <john.wilkinson@fotechsolutions.com>
2018-04-17 15:12:11 +0100

    changed transformer wire model

--- transformer/models.py
+++ transformer/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 
 class Wire(models.Model):
-    area = models.FloatField()
     diameter = models.FloatField()
-    ohms_per_m_min = models.FloatField()
-    ohms_per_m_rated = models.FloatField()
-    ohms_per_m_max = models.FloatField()
-    standard = models.CharField(max_length=1)
-    tolerance = models.FloatField()
     mass = models.FloatField()
-    grade_1_dia_min = models.FloatField()
     grade_1_dia_max = models.FloatField()
-    grade_2_dia_min = models.FloatField()
     grade_2_dia_max = models.FloatField()
 
     def resistance(self, length):
